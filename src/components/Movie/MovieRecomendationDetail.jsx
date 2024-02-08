2a9561ef64df7a8782f4a8622fd0040815424b20
jhonnatan suarez <jonathan.sm87@gmail.com>
2024-02-08 16:25:17 -0500

    navigation is added to the recommendations images
    navigation is added to the recommendations images

--- src/components/Movie/MovieRecomendationDetail.jsx
+++ src/components/Movie/MovieRecomendationDetail.jsx
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export const MovieRecomendationDetail = ({ title, genre, url }) => {
+export const MovieRecomendationDetail = ({ id, title, genre, url }) => {
     return(
         <div className="container">
             <div className="row justify-content-center">
             <div className="col-md-6">
-                <div className="card mb-4 d-flex align-items-center">
-                <img 
-                    src={url} 
-                    className="card-img-top" 
-                    alt={title}  
-                    style={{ maxWidth: '90%', height: 'auto', maxHeight: '500px', marginTop: '10px' }}
-                />
+                <div className="card hover-card mb-4 d-flex align-items-center">
+                <Link to={`/movie/${id}`}>
+                    <img 
+                        src={url} 
+                        className="card-img-top" 
+                        alt={title}  
+                        style={{ maxWidth: '90%', height: 'auto', maxHeight: '500px', marginTop: '10px' }}
+                    />
+                 </Link>
                 <div className="card-body">
                     <h6 className="card-subtitle mb-2 text-muted text-center">{genre}</h6>
                     <h5 className="card-title text-center">{title}</h5>
